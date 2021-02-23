f578b0f5145976ce33ef87e770c601cc39320d9a
DAO Hamadou <daohamadou@gmail.com>
2021-02-23 15:54:00 +0000

    '

--- wp-content/plugins/newsletter/newsletter.php
+++ wp-content/plugins/newsletter/newsletter.php
@@ -441,7 +441,7 @@ function sendMailNewLetter($visitor_email,$f_name,$l_name,$subject,$message){
     $plugin_url = "CEH";
     $headers = 'Content-type: text/html'."\r\n"."From:$plugin_url <$adminemail>"."\r\n".'Reply-To: '.$adminemail . "\r\n".'X-Mailer: PHP/' . phpversion();			
     
-    $subject =  'CEH NewsLetter '+$subject;
+    $subject =  'CEH NewsLetter '.$subject;
         $message = 'Hi '.$f_name.' '.$l_name.', <br/>'.$message;			
     global $current_user;
     wp_get_current_user();
