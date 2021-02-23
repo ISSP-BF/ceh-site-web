5443d185c254a54d3f8193050fea92af7ac90b94
DAO Hamadou <daohamadou@gmail.com>
2021-02-23 15:30:46 +0000

    '

--- wp-content/plugins/newsletter/newsletter.php
+++ wp-content/plugins/newsletter/newsletter.php
@@ -345,7 +345,7 @@ function add_subscribers(){
     endif;
     }
 }
-require_once('mail_sender.php');
+
 function sendMail($subject,$message){
     if(empty($subject))return '';
     global $wpdb;
@@ -433,3 +433,18 @@ function show_new_letters(){
     endif;
 
 }
+
+function sendMailNewLetter($visitor_email,$f_name,$l_name,$subject,$message){
+    $plugin_url = site_url();             
+				//$headers = array('From: '.$adminemail);
+    $adminemail = "newsletter@ceh-uemoa.org";
+    $plugin_url = "CEH";
+    $headers = 'Content-type: text/html'."\r\n"."From:$plugin_url <$adminemail>"."\r\n".'Reply-To: '.$adminemail . "\r\n".'X-Mailer: PHP/' . phpversion();			
+    
+    $subject =  'CEH NewsLetter '+$subject;
+        $message = 'Hi '.$f_name.' '.$l_name.', <br/>'.$message;			
+    global $current_user;
+    wp_get_current_user();
+    $mail= wp_mail( $visitor_email, $subject, $message, $headers);
+}
+
